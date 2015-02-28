0d5583b5a27babec67116197d4c85eadfae7a9e7
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-02-28 12:46:05 +0100

    Move coordinate-specific link helpers to plugin

--- glue/plugins/__init__.py
+++ glue/plugins/__init__.py
@@ -1,6 +1,5 @@
 from . import export_d3po
 from . import export_plotly
-from . import ginga_viewer
 
 
 def load_all_plugins():
@@ -10,3 +9,5 @@ def load_all_plugins():
 
     from .ginga_viewer import load_ginga_viewer_plugin
     load_ginga_viewer_plugin()
+
+from . import coordinate_helpers
