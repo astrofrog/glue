68074f7455973d558008bc6611e8a52addf765a5
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-03-16 15:29:49 +0100

    Make use of attribute limits helper in scatter options widget

--- glue/viewers/scatter/qt/options_widget.py
+++ glue/viewers/scatter/qt/options_widget.py
@@ -7,6 +7,7 @@ from glue.external.qt import QtGui
 
 from glue.utils.qt.widget_properties import CurrentComboProperty, FloatLineProperty, connect_bool_button, ButtonProperty
 from glue.utils.qt import load_ui
+from glue.viewers.common.qt.attribute_limits_helper import AttributeLimitsHelper
 
 __all__ = ["ScatterOptionsWidget"]
 
@@ -25,7 +26,7 @@ class ScatterOptionsWidget(QtGui.QWidget):
 
     hidden = FloatLineProperty('ui.checkbox_hidden')
 
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, data=None):
 
         super(ScatterOptionsWidget, self).__init__(parent=parent)
 
@@ -34,23 +35,41 @@ class ScatterOptionsWidget(QtGui.QWidget):
 
         self.ui.button_flip_axes.clicked.connect(self._flip_axes)
 
+        # Share limits cache between the two sets of limits
+        limits_cache = {}
+
+        self.x_limits_helper = AttributeLimitsHelper(self.ui.combo_x_attribute,
+                                                     self.ui.value_x_min,
+                                                     self.ui.value_x_max,
+                                                     flip_button=self.ui.button_flip_x,
+                                                     limits_cache=limits_cache)
+
+        self.y_limits_helper = AttributeLimitsHelper(self.ui.combo_y_attribute,
+                                                     self.ui.value_y_min,
+                                                     self.ui.value_y_max,
+                                                     flip_button=self.ui.button_flip_y,
+                                                     limits_cache=limits_cache)
+
+        self.x_limits_helper.data = data
+        self.y_limits_helper.data = data
+
     def _flip_axes(self):
         self.x_att, self.y_att = self.y_att, self.x_att
+        self.x_log, self.y_log = self.y_log, self.x_log
 
-
-
-
-# Have a way for cache to be shared between different axis limit helpers
-
+# TODO: include log button in attribute limits helper
 
 if __name__ == "__main__":
-    
+
     from glue.external.qt import get_qapp
-    
+    from glue.core import Data
+
+    data = Data(x=[1,2,3], y=[4,5,6], z=[7,8,9])
+
     app = get_qapp()
-    
-    widget = ScatterOptionsWidget()
+
+    widget = ScatterOptionsWidget(data=data)
     widget.show()
     widget.raise_()
-    
+
     app.exec_()
